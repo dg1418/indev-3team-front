fcf31e637cbbab7a1770e56b4de5189251e48580
Ho-S-u <jhp8655@naver.com>
2025-09-21 01:24:40 +0900

    feature/#12/App.js : MainPage와 loginPage간에 경로 설정, <LoginPage> 컴포넌트 import 구문에 추가

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,20 @@
 /**
  * @file App.js
  * @description React 애플리케이션의 최상위 컴포넌트입니다.
- *              주로 `MainPage` 컴포넌트를 렌더링하여 전체 애플리케이션의 진입점 역할을 합니다.
+ *              라우팅을 설정하여 URL 경로에 따라 적절한 페이지를 렌더링합니다.
  */
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
+import LoginPage from './loginPage'; // loginPage.js를 import합니다.
 
 function App() {
-  return <MainPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
